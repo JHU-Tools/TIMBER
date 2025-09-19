a24f4be91b500fa43df2d524e7a1c6060889d306
Xinlong <xliu5974@gmail.com>
2025-09-19 13:52:54 -0500

    Add PileUp correctionlib

---
+++ TIMBER/Tools/AutoPU_correctionlib.py
@@ -0,0 +1,37 @@
+'''
+Automatic calculation of per-jet per-event JES factors and uncertainties.
+'''
+from TIMBER.Tools.Common import GetJMETag, CompileCpp
+from TIMBER.Analyzer import Correction
+import correctionlib
+import ROOT
+
+'''
+Should you wish to use a custom-named collection derived from the FatJet or Jet
+collections, it is advisable to modify them in the script from which you call 
+AutoJME, for example:
+
+    from TIMBER.Tools.Common import AutoJME
+    AutoJME.AK8collection = "myCustomAK8collection"
+    AutoJME.AutoJME(analyzer, "myCustomAK8collection", 2017, '', True)
+'''
+
+def AutoPU(a, year):
+
+
+    json = 'puWeights'
+
+    fname = f"/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/LUM/{year}/{json}.json.gz"
+
+
+    
+    cset = correctionlib.CorrectionSet.from_file(fname)
+    keys = list(cset.keys()) # NOTE: we are using "cset_jes.compound" here b/c individual JEC levels have not been implemented for AutoJME, and we are just using the compound "L1L2L3Res" JEC level
+    print(f"keys: {keys}")
+    key = keys[0] 
+    pu = Correction("PileUp_Corr", "TIMBER/Framework/src/PileUp_correctionlib_weight.cc", [fname, key, a.isData], corrtype="weight")
+    evalargs = {
+            "Pileup_nTrueInt": "Pileup_nTrueInt"
+    }
+
+    a.AddCorrection(pu, evalargs)
