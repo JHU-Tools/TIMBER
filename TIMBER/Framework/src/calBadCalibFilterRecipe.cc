25bb40e8293687ffef751b0fdfdd72126defd4d3
Xinlong <xliu5974@gmail.com>
2025-10-19 21:42:35 -0500

    Fixed bugs.

--- TIMBER/Framework/src/calBadCalibFilterRecipe.cc
+++ TIMBER/Framework/src/calBadCalibFilterRecipe.cc
@@ -1,6 +1,5 @@
-#include <ROOT/RVec.hxx>
 
-using ROOT::VecOps::RVec;
+#include "../include/calBadCalibFilterRecipe.h"
 bool calBadCalibFilterRecipe(int run,  float PuppiMET_pt, float PuppiMET_phi,  int nJet, RVec<float> Jet_pt, RVec<float> Jet_eta, RVec<float> Jet_phi, RVec<float> Jet_neEmEF, RVec<float> Jet_chEmEF) {
     if(run >= 362433 && run <= 367144){
         if (PuppiMET_pt > 100){
