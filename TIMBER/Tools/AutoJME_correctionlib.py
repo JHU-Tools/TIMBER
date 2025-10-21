aeb06ad717fbf02f079e9370e5a13ba7ce151ede
Xinlong <xliu5974@gmail.com>
2025-10-21 14:33:51 -0500

    Fixed bugs in AutoNoiseFilter and AutoJME_corectionlib; Allowed correction for both AK4 and AK8 Jets.

--- TIMBER/Tools/AutoJME_correctionlib.py
+++ TIMBER/Tools/AutoJME_correctionlib.py
@@ -18,7 +18,7 @@ AutoJME, for example:
 AK8collection = "FatJet"
 AK4collection = "Jet"
 
-def AutoJME(a, jetCollection, year, dataEra='', calibrate=True):
+def AutoJME(a, jetCollections, year, dataEra='', calibrate=True):
     '''
     @param a (analyzer): TIMBER analyzer object to be manipulated and returned.
     @param jetCollection (str): Name of the jet collection to correct.
@@ -46,174 +46,189 @@ def AutoJME(a, jetCollection, year, dataEra='', calibrate=True):
 
     if ((a.isData) and (dataEra == '')):
         raise ValueError(f'Running on data but no dataEra specified.')
-
-    # Get the 4-digit year
-    y = int(year.split('_')[0][:4])
-
-    # Determine the jet clustering and cleaning algorithm and which JSON file to use
-    if jetCollection == AK8collection:
-        algo  = 'AK8PFPuppi'
-        json  = 'fatJet_jerc'
-        genJetColl = "GenJetAK8"
-        dRmax = 0.8
-        # Determine whether to do JMR/JMS corrections to MC
-        if (y <= 2018): # Run 2
-            doMass = True
-        else:
-            doMass = False
-    elif jetCollection == AK4collection:
+    CompileCpp('TIMBER/Framework/src/JERC_JetVeto.cc')
+    for jetCollection in jetCollections:
+        # Get the 4-digit year
         y = int(year.split('_')[0][:4])
-        if (y <= 2018): # Run 2
-            algo = 'AK4PFchs'
+
+        # Determine the jet clustering and cleaning algorithm and which JSON file to use
+        if jetCollection == AK8collection:
+            algo  = 'AK8PFPuppi'
+            json  = 'fatJet_jerc'
+            genJetColl = "GenJetAK8"
+            dRmax = 0.8
+            # Determine whether to do JMR/JMS corrections to MC
+            if (y <= 2018): # Run 2
+                doMass = True
+            else:
+                doMass = False
+        elif jetCollection == AK4collection:
+            if (y <= 2018): # Run 2
+                algo = 'AK4PFchs'
+            else:
+                algo = 'AK4PFPuppi'
+            json = 'jet_jerc'
+            genJetColl = "GenJet"
+            dRmax = 0.4
+            doMass = False
         else:
-            algo = 'AK4PFPuppi'
-        json = 'jet_jerc'
-        genJetColl = "GenJet"
-        drMax = 0.4
-        doMass = False
-    else:
-        available_colls = list(a._collectionOrg.GetCollectionNames())
-        raise ValueError(f'Jet collection name {jetCollection} not supported. Make sure to set AutoJME.AK8collection or AutoJME.AK4collection if using a custom collection. The collections available in the passed analyzer are: {available_colls}')
+            available_colls = list(a._collectionOrg.GetCollectionNames())
+            raise ValueError(f'Jet collection name {jetCollection} not supported. Make sure to set AutoJME.AK8collection or AutoJME.AK4collection if using a custom collection. The collections available in the passed analyzer are: {available_colls}')
 
-    # Determine the JEC level
-    levels = ['L1Fastjet', 'L2Relative', 'L3Absolute', 'L2L3Residual'] # NOT IMPLEMENTED YET (and not really needed for most cases)
-    level  = 'L1L2L3Res' # Currently only supporting the compound correction
-    unc    = 'Total'
+        # Determine the JEC level
+        levels = ['L1Fastjet', 'L2Relative', 'L3Absolute', 'L2L3Residual'] # NOT IMPLEMENTED YET (and not really needed for most cases)
+        level  = 'L1L2L3Res' # Currently only supporting the compound correction
+        unc    = 'Total'
 
-    # Load the CorrectionSet from the file hosted on CVMFS. These files are synced daily, see here: https://gitlab.cern.ch/cms-nanoAOD/jsonpog-integration/-/blob/master/README.md
-    fname_jes = f"/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/JME/{year}/{json}.json.gz"
+        # Load the CorrectionSet from the file hosted on CVMFS. These files are synced daily, see here: https://gitlab.cern.ch/cms-nanoAOD/jsonpog-integration/-/blob/master/README.md
+        fname_jes = f"/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/JME/{year}/{json}.json.gz"
 
-    print(f'\nJet collection: {jetCollection}')
-    print(f'Jet algorithm:  {algo}')
-    print(f'Uncertainty:    {unc}')
-    print(f'JEC level:      {level}')
-    print(f'Year:           {year}')
-    if (a.isData):
-        print(f'Era:            {dataEra}')
+        print(f'\nJet collection: {jetCollection}')
+        print(f'Jet algorithm:  {algo}')
+        print(f'Uncertainty:    {unc}')
+        print(f'JEC level:      {level}')
+        print(f'Year:           {year}')
+        if (a.isData):
+            print(f'Era:            {dataEra}')
 
-    print("\nLoading JSON file: {}".format(fname_jes))
-    cset_jes = core.CorrectionSet.from_file(fname_jes)
-    keys = list(cset_jes.compound.keys()) # NOTE: we are using "cset_jes.compound" here b/c individual JEC levels have not been implemented for AutoJME, and we are just using the compound "L1L2L3Res" JEC level
-    print(keys)
-    # Find the appropriate CorrectionSet key for Data or MC
-    if (a.isData):
-        found = False
-        keysData = [k for k in keys if 'DATA' in k]
-        if "2022" in year:
-            for k in keysData:
-                idx_start = k.find("Run") 
-                idx_end = k.find("_", idx_start)
-                era = k[idx_start : idx_end]
-                if dataEra in era: 
-                    found = True
-                    key = k
-                    break
-            if not found:
-                raise ValueError(f'The dataEra {dataEra} does not correspond with any keys in the JSON CorrectionSet. Available data keys are: {keysData}')            
-        elif "2023" in year:
-            key = keysData[0]
-    else:
-        # There is only one compound key in the JSON for MC
-        key = [k for k in keys if 'MC' in k][0]
-
-    print(f'\nUsing compound JEC level key: "{key}"')
-    print(f'\nUsing compound JEC uncertainty\n')
-
-    jes = Calibration(
-        f"{jetCollection}_JES",
-        "TIMBER/Framework/src/JES_correctionlib_weight.cc", 
-        [fname_jes, key, key.replace(level,unc), a.isData], 
-        corrtype='Calibration'
-    )
-    evalargs = {
-        jes: {
-            "pt": f"{jetCollection}_pt",
-            "eta": f"{jetCollection}_eta",
-            "phi": f"{jetCollection}_phi",
-            "area": f"{jetCollection}_area",
-            "run":  "run",
-            "fixedGridRhoFastjetAll":"fixedGridRhoFastjetAll" if (y <= 2018) else "Rho_fixedGridRhoFastjetAll"
-        }
-    }
-
-    calibdict = {
-        f"{jetCollection}_pt":[jes],
-        f"{jetCollection}_mass":[jes],
-        f"{jetCollection}_msoftdrop":[jes]
-    }
-
-    # Create the columns corresponding to the JES variations
-    if (a.isData):
-        if (calibrate):
-            a.CalibrateVars(calibdict,evalargs,'',variationsFlag=(not a.isData))
+        print("\nLoading JSON file: {}".format(fname_jes))
+        cset_jes = core.CorrectionSet.from_file(fname_jes)
+        keys = list(cset_jes.compound.keys()) # NOTE: we are using "cset_jes.compound" here b/c individual JEC levels have not been implemented for AutoJME, and we are just using the compound "L1L2L3Res" JEC level
+        print(keys)
+        # Find the appropriate CorrectionSet key for Data or MC
+        if (a.isData):
+            found = False
+            keysData = [k for k in keys if 'DATA' in k]
+            if "2022" in year:
+                for k in keysData:
+                    idx_start = k.find("Run") 
+                    idx_end = k.find("_", idx_start)
+                    era = k[idx_start : idx_end]
+                    if dataEra in era: 
+                        found = True
+                        key = k
+                        break
+                if not found:
+                    raise ValueError(f'The dataEra {dataEra} does not correspond with any keys in the JSON CorrectionSet. Available data keys are: {keysData}')            
+            elif "2023" in year:
+                key = keysData[0]
         else:
-            a.CalibrateVars({},evalargs,'',variationsFlag=(not a.isData))
-    else:
-        # Don't calibrate yet until JER is calcuated
-        a.CalibrateVars({},evalargs,'',variationsFlag=(not a.isData))
+            # There is only one compound key in the JSON for MC
+            key = [k for k in keys if 'MC' in k][0]
 
-    # Now handle JER corrections to MC only. JER corrections use the JES-corrected pT
-    if not a.isData:
-        print(f'\nStep 2: JER corrections...\n')
+        print(f'\nUsing compound JEC level key: "{key}"')
+        print(f'\nUsing compound JEC uncertainty\n')
 
-        # Get the appropriate keys for the JER resolution and SF. These will be stored in the JES correctionset "cset_jes"
-        key_res = [i for i in cset_jes if 'PtResolution' in i][0]   # Each correctionset has only one of these keys, so the list will always be one element long
-        key_sf  = [i for i in cset_jes if 'ScaleFactor' in i][0]
-
-        # Multiplicative factor to the difference b/w the GEN and RECO jet pT, used in the JER correction algorithm (see JER_correctionlib_weight.cc)
-        dpTmax = 3
-
-        print(f'Using JER resolution key "{key_res}"')
-        print(f'Using JER SF key         "{key_sf}"')
-        print(f'Using hybrid smearing method with GEN match parameters:')
-        print(f'\t delta pT max factor = {dpTmax}')
-        print(f'\t deltaR max          = {dRmax}\n')
-
-        jer = Calibration(
-            f"{jetCollection}_JER",
-            'TIMBER/Framework/src/JER_correctionlib_weight.cc',
-            [
-                fname_jes,  # Name of the jerc file for AK8 or AK4
-                key,        # JES key
-                key_res,    # pt resolution key
-                key_sf,     # SF key
-                dRmax,      # used for gen<->reco matching. 0.8 for AK8, 0.4 for AK4
-                dpTmax      # dPtMaxFactor, default for CMS
-            ],
+        jes = Calibration(
+            f"{jetCollection}_JES",
+            "TIMBER/Framework/src/JES_correctionlib_weight.cc", 
+            [fname_jes, key, key.replace(level,unc), a.isData], 
             corrtype='Calibration'
         )
-
         evalargs = {
-            jer: {
-                "jets":f"{jetCollection}s",
-                "genJets":f"{genJetColl}s",
+            jes: {
+                "pt": f"{jetCollection}_pt",
+                "eta": f"{jetCollection}_eta",
+                "phi": f"{jetCollection}_phi",
+                "area": f"{jetCollection}_area",
+                "run":  "run",
                 "fixedGridRhoFastjetAll":"fixedGridRhoFastjetAll" if (y <= 2018) else "Rho_fixedGridRhoFastjetAll"
             }
         }
+        if jetCollection == AK8collection:    
+            calibdict = {
+                f"{jetCollection}_pt":[jes],
+                f"{jetCollection}_mass":[jes],
+                f"{jetCollection}_msoftdrop":[jes]
+            }
+        elif jetCollection == AK4collection:
+            calibdict = {
+                f"{jetCollection}_pt":[jes],
+                f"{jetCollection}_mass":[jes],
+            }
+        
 
-        calibdict = {
-            f"{jetCollection}_pt":[jes,jer],
-            f"{jetCollection}_mass":[jes,jer],
-            f"{jetCollection}_msoftdrop":[jes,jer]
-        }
-
-        if (calibrate):
-            a.CalibrateVars(calibdict,evalargs,'',variationsFlag=(not a.isData))
+        # Create the columns corresponding to the JES variations
+        if (a.isData):
+            if (calibrate):
+                a.CalibrateVars(calibdict,evalargs,'',variationsFlag=(not a.isData))
+            else:
+                a.CalibrateVars({},evalargs,'',variationsFlag=(not a.isData))
         else:
+            # Don't calibrate yet until JER is calcuated
             a.CalibrateVars({},evalargs,'',variationsFlag=(not a.isData))
 
-    # Now apply veto maps to Data and MC (Run 3 ONLY)
-    if (y > 2018):
-        print('\nStep 3: Applying JERC jet veto maps (Run 3 only)...')
-        fname_vetomap = f"/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/JME/{year}/jetvetomaps.json.gz"
-        cset_vetomap = core.CorrectionSet.from_file(fname_vetomap)
-        key_vetomap = [k for k in cset_vetomap][0]  # there is only one vetomap key, so the key will always be the first and only element
-        CompileCpp('TIMBER/Framework/src/JERC_JetVeto.cc')
-        CompileCpp(f'JERC_JetVeto jet_vetoer = JERC_JetVeto("{fname_vetomap}","{key_vetomap}");')
-        a.Define('jetmap_vetoed_events',f'jet_vetoer.eval(Jets)')   # Pass in the TIMBER-created struct for the AK4 jets ("Jet"+"s")
-        a.Cut('JERC_jet_veto','jetmap_vetoed_events == 0')
 
 
-    print('\n----------------------------------------------------------------------------------------')
-    print('------------------------------Finished AutoJME -----------------------------------------')
-    print('----------------------------------------------------------------------------------------')
+
+        # Now handle JER corrections to MC only. JER corrections use the JES-corrected pT
+        if not a.isData:
+            print(f'\nStep 2: JER corrections...\n')
+
+            # Get the appropriate keys for the JER resolution and SF. These will be stored in the JES correctionset "cset_jes"
+            key_res = [i for i in cset_jes if 'PtResolution' in i][0]   # Each correctionset has only one of these keys, so the list will always be one element long
+            key_sf  = [i for i in cset_jes if 'ScaleFactor' in i][0]
+
+            # Multiplicative factor to the difference b/w the GEN and RECO jet pT, used in the JER correction algorithm (see JER_correctionlib_weight.cc)
+            dpTmax = 3
+
+            print(f'Using JER resolution key "{key_res}"')
+            print(f'Using JER SF key         "{key_sf}"')
+            print(f'Using hybrid smearing method with GEN match parameters:')
+            print(f'\t delta pT max factor = {dpTmax}')
+            print(f'\t deltaR max          = {dRmax}\n')
+
+            jer = Calibration(
+                f"{jetCollection}_JER",
+                'TIMBER/Framework/src/JER_correctionlib_weight.cc',
+                [
+                    fname_jes,  # Name of the jerc file for AK8 or AK4
+                    key,        # JES key
+                    key_res,    # pt resolution key
+                    key_sf,     # SF key
+                    dRmax,      # used for gen<->reco matching. 0.8 for AK8, 0.4 for AK4
+                    dpTmax      # dPtMaxFactor, default for CMS
+                ],
+                corrtype='Calibration'
+            )
+
+            evalargs = {
+                jer: {
+                    "jets":f"{jetCollection}s",
+                    "genJets":f"{genJetColl}s",
+                    "fixedGridRhoFastjetAll":"fixedGridRhoFastjetAll" if (y <= 2018) else "Rho_fixedGridRhoFastjetAll"
+                }
+            }
+
+            if jetCollection == AK8collection:    
+                calibdict = {
+                    f"{jetCollection}_pt":[jes, jer],
+                    f"{jetCollection}_mass":[jes, jer],
+                    f"{jetCollection}_msoftdrop":[jes, jer]
+                }
+            elif jetCollection == AK4collection:
+                calibdict = {
+                    f"{jetCollection}_pt":[jes, jer],
+                    f"{jetCollection}_mass":[jes, jer],
+                }
+
+            if (calibrate):
+                a.CalibrateVars(calibdict,evalargs,'',variationsFlag=(not a.isData))
+            else:
+                a.CalibrateVars({},evalargs,'',variationsFlag=(not a.isData))
+
+        # Now apply veto maps to Data and MC (Run 3 ONLY)
+        if (y > 2018):
+            print('\nStep 3: Applying JERC jet veto maps (Run 3 only)...')
+            fname_vetomap = f"/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/JME/{year}/jetvetomaps.json.gz"
+            cset_vetomap = core.CorrectionSet.from_file(fname_vetomap)
+            key_vetomap = [k for k in cset_vetomap][0]  # there is only one vetomap key, so the key will always be the first and only element
+            CompileCpp(f'JERC_JetVeto {jetCollection}_jet_vetoer = JERC_JetVeto("{fname_vetomap}","{key_vetomap}");')
+            #a.Define(f'{jetCollection}_jetmap_vetoed_events',f'{jetCollection}_jet_vetoer.eval({jetCollection}s)')   # Pass in the TIMBER-created struct for the AK4 jets ("Jet"+"s")
+            a.Define(f'{jetCollection}_jetmap_vetoed_events',f'{jetCollection}_jet_vetoer.eval(Jets)')   # Pass in the TIMBER-created struct for the AK4 jets ("Jet"+"s")
+            a.Cut(f'{jetCollection}_JERC_jet_veto',f'{jetCollection}_jetmap_vetoed_events == 0')
+
+
+        print('\n----------------------------------------------------------------------------------------')
+        print('------------------------------Finished AutoJME -----------------------------------------')
+        print('----------------------------------------------------------------------------------------')
--- TIMBER/Tools/AutoNoiseFilter.py
+++ TIMBER/Tools/AutoNoiseFilter.py
@@ -1,9 +1,12 @@
-import ROOT
 
-def ApplyNoiseFilter(analyzer, year, isData):
+import ROOT
+from TIMBER.Tools.Common import CompileCpp
+CompileCpp('TIMBER/Framework/src/ecalBadCalibFilterRecipe.cc')
+
+def AutoNoiseFilter(analyzer, year, isData):
     if year == "2022" or year == "2022EE" or year == "2023" or year == "2023BPix" or year == "2024":
         filterString = "Flag_goodVertices == 1 && Flag_globalSuperTightHalo2016Filter == 1 && Flag_EcalDeadCellTriggerPrimitiveFilter== 1 && Flag_BadPFMuonFilter == 1 && Flag_BadPFMuonDzFilter == 1 && Flag_hfNoisyHitsFilter == 1 && Flag_eeBadScFilter == 1"
-        analyzer.Cut(f"NoiseFilters", filterString)
+        analyzer.Cut("NoiseFilters", filterString)
         if (year == "2022" or year == "2022EE" or year == "2023" or year == "2023BPix") and isData:
             analyzer.Cut("ecalBadCalibFilter", "ecalBadCalibFilterRecipe(run,  PuppiMET_pt, PuppiMET_phi,  nJet, Jet_pt, Jet_eta, Jet_phi, Jet_neEmEF, Jet_chEmEF)")
         elif year == 2024:
